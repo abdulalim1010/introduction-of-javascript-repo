7fbe9e3b1370295975e4ba00047a995bee90eca6
abdulalilm1010 <abdulalim243457@gmail.com>
2025-02-04 22:40:45 +0600

    main problem

---
+++ number.js
@@ -0,0 +1,10 @@
+var orangePrice = 44;
+var choklet = .6;
+
+var mango =parseInt ("60");
+console.log(orangePrice)
+console.log(choklet + orangePrice + mango)
+var nume = .1;
+var name3 = .2;
+var total = nume + name3;
+console.log(total.toFixed(4))
