ba9fc781e35c659bc202700e20cf7b41f532faef
abdulalilm1010 <abdulalim243457@gmail.com>
2025-02-04 22:02:32 +0600

    name first

---
+++ types.js
@@ -0,0 +1,7 @@
+var price = 55;
+var name = "alim";
+var passed = true;
+console.log(passed)
+console.log(name)
+console.log(typeof name)
+console.log(typeof price)
---
+++ variable.js
@@ -0,0 +1,6 @@
+/* dsfhjk;dsfkj */
+/*  */
+var age = 24;
+age = 35;
+console.log(age)
+console.log(age)
